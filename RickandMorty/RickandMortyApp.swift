8d4bbbf62dca9f1e4dc159d75156a8dbd49f3285
Naol Hussien <naol.h@suntecholding.com>
2022-12-08 10:34:47 +0300

    Added: initial

---
+++ RickandMorty/RickandMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickandMortyApp.swift
+//  RickandMorty
+//
+//  Created by Naol Hassen on 08/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct RickandMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
